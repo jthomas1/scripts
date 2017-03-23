f5e34e4f7ac8a1ab1140a3f5c1d703d10749a8c3
James Thomas <plinkplonk@gmail.com>
2017-03-23 19:17:03 +0000

    added pulse audio volume

--- status_bar.py
+++ status_bar.py
@@ -5,6 +5,9 @@ status = Status()
 status.register("clock",
                 format="%a %-d %b %X KW%V")
 
+status.register("pulseaudio",
+    format="♪{volume}",)
+
 status.register("load")
 
 status.run()
